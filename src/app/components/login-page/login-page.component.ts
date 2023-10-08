4489376ef81b126cb1e6e9f04029a5fda7481462
Jonathan Rangel <jonathanrangelb@gmail.com>
2023-10-08 00:52:19 -0600

    chore: cambio de login page

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -12,21 +12,35 @@ export class LoginPageComponent {
   userData: any
   user: string = ""
   password: string = ""
+  error: string = ""
 
   constructor(private authService: AuthService) { }
 
   requestLogin() {
+    // stores auth token on local storage
     // const token = localStorage.getItem('token')
     // if (token) {
     //   console.warn(token)
     // }
 
+    // clean up the messages of previous calls
+    this.error = ""
+    this.userData = undefined
+
     this.authService.login(this.user, this.password.toUpperCase()).subscribe((usr: any) => {
       this.userData = usr
       localStorage.setItem('token', usr.Autorization)
     }, error => {
-      if (error instanceof HttpErrorResponse)
-        this.userData = { "message": error.error?.message, "statusCode": error.status }
+      if (error instanceof HttpErrorResponse) {
+        if (error.status == 404) {
+          this.error = error.error?.message
+          console.log(this.error);
+        }
+        else {
+          this.error = "Ocurrio un error, intenta nuevamente"
+          console.log(this.error);
+        }
+      }
     })
 
   }
