ec5f362c8ed97392304e5cae95c99b739fc7f0cd
Jonathan Rangel <jonathanrangelb@gmail.com>
2023-12-08 00:49:00 -0600

    fix: removed test file for pagoscomponent

--- src/app/components/login-page/login-page.component.spec.ts
+++ src/app/components/login-page/login-page.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginPageComponent } from './login-page.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
@@ -11,7 +12,7 @@ describe('LoginPageComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [LoginPageComponent],
-      imports: [HttpClientModule, FormsModule],
+      imports: [HttpClientModule, FormsModule, ButtonModule],
     });
     fixture = TestBed.createComponent(LoginPageComponent);
     component = fixture.componentInstance;
