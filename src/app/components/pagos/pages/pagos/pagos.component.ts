1a6fea6116b1cb82e511a092815dd3d53c99ad24
Jonathan Rangel <jonathanrangelb@gmail.com>
2023-12-24 17:52:29 -0600

    refactor: payment handling in PagosComponent

--- src/app/components/pagos/pages/pagos/pagos.component.ts
+++ src/app/components/pagos/pages/pagos/pagos.component.ts
@@ -4,6 +4,8 @@ import { Component, OnInit } from '@angular/core';
 import { PagosService } from 'src/app/services/pagos-service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Prestamos } from 'src/app/models/db/prestamos';
+import { PrestamoConDetallesCompletos } from '../../../../models/db/prestamos';
+import { SPAltaPago } from 'src/app/models/storedProcedures/SPAltaPago';
 
 @Component({
   selector: 'app-pagos',
@@ -48,55 +50,58 @@ export class PagosComponent implements OnInit {
   pagar(item: PrestamosDetalle): void {
     this.confirmationService.confirm({
       message: `¿Estás seguro de pagar ${item.CANTIDAD} correspondientes a la semana ${item.NUMERO_SEMANA} para el folio ${item.ID_PRESTAMO}?`,
-      accept: () => this.pagoExitoso(item),
-      reject: () => this.pagoCancelado(item),
+      accept: () => this.registrarPago(item),
+      reject: () => this.rechazarPago(item),
     });
   }
 
-  pagoExitoso(item: PrestamosDetalle): void {
-    this.pagosService
-      .pay({
-        ID_PRESTAMO: +item.ID_PRESTAMO,
-        ID_MULTA: 0,
-        NUMERO_SEMANA: item.NUMERO_SEMANA,
-        ID_CLIENTE: this.numeroDeCliente,
-        ID_USUARIO: +(localStorage.getItem('idusuario') || ''),
-        CANTIDAD_PAGADA: item.CANTIDAD,
-        FECHA_ACTUAL: new Date(),
-        ID_COBRADOR: +(localStorage.getItem('idusuario') || ''),
-      })
-      .subscribe({
-        next: () => {
-          this.prestamosDetalle = this.prestamosDetalle.map((pago) => {
-            if (pago.NUMERO_SEMANA === item.NUMERO_SEMANA) {
-              return {
-                ...pago,
-                STATUS: 'PAGADO',
-              };
-            } else return pago;
-          });
-
-          this.messageService.add({
-            severity: 'success',
-            summary: 'Pago exitoso',
-            detail: `El pago correspondiendte a ${item.NUMERO_SEMANA} del folio ${item.ID_PRESTAMO} se ha registrado`,
-            icon: 'pi pi-check',
-            life: 5000,
-          });
-        },
-        error: (err) => {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: `Ocurrio un error al interntar registrar el pago: ${err.error.message}`,
-            icon: 'pi pi-exclamation-triangle',
-            life: 5000,
-          });
-        },
-      });
+  registrarPago(item: PrestamosDetalle): void {
+    const sPAltaPago: SPAltaPago = {
+      ID_PRESTAMO: +item.ID_PRESTAMO,
+      ID_MULTA: 0,
+      NUMERO_SEMANA: item.NUMERO_SEMANA,
+      ID_CLIENTE: this.numeroDeCliente,
+      ID_USUARIO: +(localStorage.getItem('idusuario') || ''),
+      CANTIDAD_PAGADA: item.CANTIDAD,
+      FECHA_ACTUAL: new Date(),
+      ID_COBRADOR: +(localStorage.getItem('idusuario') || ''),
+    };
+    this.pagosService.pay(sPAltaPago).subscribe({
+      next: () => this.resgistrarPagoExitoso(item),
+      error: (err) => this.errorAlRegistrarPago(err),
+    });
   }
 
-  pagoCancelado(item: PrestamosDetalle): void {
+  resgistrarPagoExitoso(item: PrestamosDetalle) {
+    this.prestamosDetalle = this.prestamosDetalle.map((pago) => {
+      if (pago.NUMERO_SEMANA === item.NUMERO_SEMANA) {
+        return {
+          ...pago,
+          STATUS: 'PAGADO',
+        };
+      } else return pago;
+    });
+
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Pago exitoso',
+      detail: `El pago correspondiendte a ${item.NUMERO_SEMANA} del folio ${item.ID_PRESTAMO} se ha registrado`,
+      icon: 'pi pi-check',
+      life: 5000,
+    });
+  }
+
+  errorAlRegistrarPago(err: any): void {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: `Ocurrio un error al interntar registrar el pago: ${err.error.message}`,
+      icon: 'pi pi-exclamation-triangle',
+      life: 5000,
+    });
+  }
+
+  rechazarPago(item: PrestamosDetalle): void {
     this.messageService.add({
       severity: 'warn',
       summary: 'Acción cancelada',
@@ -110,37 +115,40 @@ export class PagosComponent implements OnInit {
     this.loading = true;
     this.hiddenTable = false;
     this.pagosService.getPaymentsById(this.folioForm.value.folio).subscribe({
-      next: (pagos) => {
-        console.warn(pagos);
-        if (!pagos.prestamos) {
-          this.prestamosDetalle = [];
-          this.prestamo = undefined;
-          this.messageService.add({
-            severity: 'warn',
-            summary: 'No se encontro el folio',
-            detail: 'El folio no existe o no esta asignado a su usuario',
-            icon: 'pi pi-exclamation-triangle',
-            life: 5000,
-          });
-        } else {
-          this.prestamosDetalle = pagos.prestamosDetalle;
-          this.prestamo = pagos.prestamos;
-          this.numeroDeCliente = pagos.prestamos.ID_CLIENTE;
-        }
-        this.loading = false;
-      },
-      error: (err) => {
-        console.log(err);
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: 'No se pudo obtener la información del folio',
-          icon: 'pi pi-exclamation-triangle',
-          life: 5000,
-        });
-        this.prestamo = undefined;
-        this.loading = false;
-      },
+      next: (pagos) => this.datosDelFolio(pagos),
+      error: (err) => this.errorEnDatosDelFolio(err),
     });
+  }
+
+  datosDelFolio(pagos: PrestamoConDetallesCompletos): void {
+    if (!pagos.prestamos) {
+      this.prestamosDetalle = [];
+      this.prestamo = undefined;
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'No se encontro el folio',
+        detail: 'El folio no existe o no esta asignado a su usuario',
+        icon: 'pi pi-exclamation-triangle',
+        life: 5000,
+      });
+    } else {
+      this.prestamosDetalle = pagos.prestamosDetalle;
+      this.prestamo = pagos.prestamos;
+      this.numeroDeCliente = pagos.prestamos.ID_CLIENTE;
+    }
+    this.loading = false;
+  }
+
+  errorEnDatosDelFolio(err: unknown): void {
+    console.log(err);
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: 'No se pudo obtener la información del folio',
+      icon: 'pi pi-exclamation-triangle',
+      life: 5000,
+    });
+    this.prestamo = undefined;
+    this.loading = false;
   }
 }
