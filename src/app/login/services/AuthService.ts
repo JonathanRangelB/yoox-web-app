1aa1667e0ff58f4af0985c66459da26f6fbac09d
Jonathan Rangel <jonathanrangelb@gmail.com>
2024-04-02 19:38:30 -0600

    refactor: mejora en authService y login-page

--- src/app/login/services/AuthService.ts
+++ src/app/login/services/AuthService.ts
@@ -1,30 +1,36 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { shareReplay } from 'rxjs';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap } from 'rxjs';
 import { UserData } from '../../shared/interfaces/userData.interface';
 import { TokenValidationResponse } from '../interfaces/tokenValidationResponse';
 import { environment } from 'src/environments/environment';
-import { User } from 'src/app/shared/interfaces/user.interface';
+import { UserCredentials } from 'src/app/shared/interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private readonly API_URL = environment.API_URL;
+  private http = inject(HttpClient);
+  private readonly baseUrl = environment.API_URL;
+  private userData?: UserData;
 
-  constructor(private http: HttpClient) {}
-
-  login({ userId, password }: User) {
+  login({ userId, password }: UserCredentials) {
     return this.http
-      .post<UserData>(`${this.API_URL}login`, { userId, password })
-      .pipe(shareReplay());
+      .post<UserData>(`${this.baseUrl}login`, { userId, password })
+      .pipe(tap((response) => (this.userData = response)));
   }
 
   tokenValidation(token: string) {
-    return this.http
-      .get<TokenValidationResponse>(`${this.API_URL}tokenValidation`, {
-        headers: { authorization: token },
-      })
-      .pipe(shareReplay());
+    const headers = new HttpHeaders().set('authorization', token);
+    return this.http.get<TokenValidationResponse>(
+      `${this.baseUrl}tokenValidation`,
+      {
+        headers,
+      }
+    );
+  }
+
+  get currentUser() {
+    return this.userData;
   }
 }
