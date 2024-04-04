62fecf41d377ed8a420f8f766b56e830ec5c76b1
Jonathan Rangel <jonathanrangelb@gmail.com>
2024-04-03 23:04:53 -0600

    chore: limpliando logs inecesarios

--- scripts/set-env.js
+++ scripts/set-env.js
@@ -11,15 +11,5 @@ export const environment = {
 };
 `;
 
-if (!process.env.PRODUCTION) {
-  console.log('PRODUCTION is not defined.');
-}
-if (!process.env.ENV_NAME) {
-  console.log('ENV_NAME is not defined.');
-}
-if (!process.env.API_URL) {
-  console.log('API_URL is not defined.');
-}
-
 mkdirSync('./src/environments', { recursive: true });
 writeFileSync(targetPath, envFileContent);
