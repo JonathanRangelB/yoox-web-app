28f891f62ff4f628c517a8cb9e1b29807b48acb8
Jonathan Rangel <jonathanrangelb@gmail.com>
2024-08-13 16:28:26 -0600

    chore: update de paquetes y script
    se hizo un ng update para corregir las vulnerabilidades encontradas por
    pnpm y tambien se actualizo el script de purgecss para mostrar el ratio
    de compresion de los archivos css cuando se hace un ng build

--- scripts/purgecss.js
+++ scripts/purgecss.js
@@ -13,10 +13,10 @@ if (!files && files.length <= 0) {
   return;
 }
 
-for (let f of files) {
+for (let file of files) {
   // get original file size
-  const originalSize = getFilesizeInKiloBytes(buildDir + f) + 'kb';
-  const o = { file: f, originalSize: originalSize, newSize: '' };
+  const originalSize = getFilesizeInKiloBytes(buildDir + file) + 'kb';
+  const o = { file, originalSize };
   data.push(o);
 }
 
@@ -25,11 +25,13 @@ console.log('Run PurgeCSS...');
 exec(
   // 'purgecss -css dist/**/*.css --content dist/**/index.html dist/**/*.js -o ./',
   'npx purgecss -c ./purgecss.config.js',
-  function (error, stdout, stderr) {
+  function () {
     for (let d of data) {
       // get new file size
-      const newSize = getFilesizeInKiloBytes(buildDir + d.file) + 'kb';
-      d.newSize = newSize;
+      const newSize = getFilesizeInKiloBytes(buildDir + d.file);
+      const compression = getReductionPercentage(d.originalSize.slice(0, -2), newSize);
+      d.newSize = newSize + 'kb';
+      d.compressedRatio = compression;
     }
     console.log('PurgeCSS done');
     console.table(data);
@@ -37,12 +39,20 @@ exec(
 );
 
 function getFilesizeInKiloBytes(filename) {
-  const stats = fs.statSync(filename);
-  const fileSizeInBytes = stats.size / 1024;
+  const stats = statSync(filename);
+  const fileSizeInBytes = stats.size / 1000;
   return fileSizeInBytes.toFixed(2);
 }
 
 function getFilesFromPath(dir, extension) {
-  let files = fs.readdirSync(dir);
-  return files.filter((e) => path.extname(e).toLowerCase() === extension);
+  let files = readdirSync(dir);
+  return files.filter((e) => extname(e).toLowerCase() === extension);
+}
+
+function getReductionPercentage(initial, compressed) {
+  console.log(initial, compressed);
+  initial = +initial;
+  compressed = +compressed;
+  const compressionRatio = (((initial - compressed) / initial) * 100).toFixed(2)
+  return `${compressionRatio}%`
 }
