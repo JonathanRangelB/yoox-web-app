524aab1e5ea6d3e1c638e1102542197d651382a9
Jonathan Rangel <jonathanrangelb@gmail.com>
2024-11-06 13:27:46 -0600

    refactor: formularios anidados para esclarecer jerarquia de valores del formulario principal

--- src/app/loan-request/pages/new-loan/new-loan.component.ts
+++ src/app/loan-request/pages/new-loan/new-loan.component.ts
@@ -1,6 +1,11 @@
 /* eslint-disable no-useless-escape */
 import { Component, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { DropdownChangeEvent } from 'primeng/dropdown';
 import { InputNumberInputEvent } from 'primeng/inputnumber';
@@ -11,14 +16,14 @@ import {
   tiposCalle,
 } from '../../utils/consts';
 import {
-  genericAsyncValidator,
   curpValidator,
   emailValidator,
   lengthValidator,
 } from '../../utils/customValidators';
 import { S3BucketService } from '../../services/s3-bucket.service';
-import { Subject, takeUntil } from 'rxjs';
+import { BehaviorSubject, skip, Subject, takeUntil } from 'rxjs';
 import { FileUpload } from 'primeng/fileupload';
+import { InputSwitchChangeEvent } from 'primeng/inputswitch';
 
 interface dropDownCollection {
   name: string;
@@ -53,82 +58,75 @@ export class NewLoanComponent implements OnInit, OnDestroy {
   tasaDeInteres: number = 0;
   totalAPagar: number = 0;
   pagoSemanal: number | null = null;
-  camposAval = false;
+  camposAval$ = new BehaviorSubject<boolean>(false);
 
   constructor() {
     this.mainForm = this.fb.group({
-      cantidad: [
-        '',
-        [Validators.required, Validators.min(1000)],
-        genericAsyncValidator(),
-      ],
+      cantidad: ['', [Validators.required, Validators.min(1000)]],
       plazos: ['', Validators.required],
       fechaInicial: ['', Validators.required],
-      nombreCliente: ['', Validators.required],
-      apellidoPaternoCliente: ['', Validators.required],
-      apellidoMaternoCliente: ['', Validators.required],
-      telefonoFijoCliente: ['', [Validators.required]],
-      telefonoMovilCliente: ['', [Validators.required]],
-      correoCliente: ['', [Validators.required, emailValidator()]],
-      ocupacionCliente: [''],
-      curpCliente: ['', [Validators.required, curpValidator()]],
-      tipoCalleCliente: [null, Validators.required],
-      nombreCalleCliente: ['', Validators.required],
-      numeroExteriorCliente: [null, Validators.required],
-      numeroInteriorCliente: [null],
-      coloniaCliente: ['', Validators.required],
-      municipioCliente: ['', Validators.required],
-      estadoCliente: ['', Validators.required],
-      codigoPostalCliente: [null, [Validators.required, lengthValidator(5)]],
-      observacionesCliente: [''],
-      // campos opcionales de Aval
-      habilitarCamposAval: [false],
-      nombreAval: [''],
-      apellidoPaternoAval: [''],
-      apellidoMaternoAval: [''],
-      telefonoFijoAval: [''],
-      telefonoMovilAval: [''],
-      emailAval: [''],
-      ocupacionAval: [''],
-      curpAval: [''],
-      tipoCalleAval: [null],
-      nombreCalleAval: [''],
-      numeroExteriorAval: [null],
-      numeroInteriorAval: [null],
-      coloniaAval: [''],
-      municipioAval: [''],
-      estadoAval: [''],
-      codigoPostalAval: [null],
-      observacionesAval: [''],
-      nuevoForm: this.fb.group({
-        algo: [''],
+      formCliente: this.fb.group({
+        nombreCliente: ['', Validators.required],
+        apellidoPaternoCliente: ['', Validators.required],
+        apellidoMaternoCliente: ['', Validators.required],
+        telefonoFijoCliente: ['', [Validators.required]],
+        telefonoMovilCliente: ['', [Validators.required]],
+        correoCliente: ['', [Validators.required, emailValidator()]],
+        ocupacionCliente: [''],
+        curpCliente: ['', [Validators.required, curpValidator()]],
+        tipoCalleCliente: [null, Validators.required],
+        nombreCalleCliente: ['', Validators.required],
+        numeroExteriorCliente: [null, Validators.required],
+        numeroInteriorCliente: [null],
+        coloniaCliente: ['', Validators.required],
+        municipioCliente: ['', Validators.required],
+        estadoCliente: ['', Validators.required],
+        codigoPostalCliente: [null, [Validators.required, lengthValidator(5)]],
+        observacionesCliente: [''],
+      }),
+      formAval: this.fb.group({
+        nombreAval: [''],
+        apellidoPaternoAval: [''],
+        apellidoMaternoAval: [''],
+        telefonoFijoAval: [''],
+        telefonoMovilAval: [''],
+        emailAval: [''],
+        ocupacionAval: [''],
+        curpAval: [''],
+        tipoCalleAval: [''],
+        nombreCalleAval: [''],
+        numeroExteriorAval: [''],
+        numeroInteriorAval: [''],
+        coloniaAval: [''],
+        municipioAval: [''],
+        estadoAval: [''],
+        codigoPostalAval: [''],
+        observacionesAval: [''],
       }),
     });
   }
 
   ngOnInit(): void {
-    this.mainForm
-      .get('habilitarCamposAval')
-      ?.valueChanges.pipe(takeUntil(this.destroy$))
-      .subscribe(() => {
-        this.camposAval = !this.camposAval;
+    this.camposAval$
+      .pipe(skip(1), takeUntil(this.destroy$))
+      .subscribe((mostrarCamposAval) => {
+        const formAval = this.mainForm.get('formAval')! as FormGroup;
+        const nombreAval = formAval.get('nombreAval')!;
+        const apellidoPaternoAval = formAval.get('apellidoPaternoAval')!;
+        const apellidoMaternoAval = formAval.get('apellidoMaternoAval')!;
+        const telefonoFijoAval = formAval.get('telefonoFijoAval')!;
+        const telefonoMovilAval = formAval.get('telefonoMovilAval')!;
+        const emailAval = formAval.get('emailAval')!;
+        const curpAval = formAval.get('curpAval')!;
+        const tipoCalleAval = formAval.get('tipoCalleAval')!;
+        const nombreCalleAval = formAval.get('nombreCalleAval')!;
+        const numeroExteriorAval = formAval.get('numeroExteriorAval')!;
+        const coloniaAval = formAval.get('coloniaAval')!;
+        const municipioAval = formAval.get('municipioAval')!;
+        const estadoAval = formAval.get('estadoAval');
+        const codigoPostalAval = formAval.get('codigoPostalAval')!;
 
-        const nombreAval = this.mainForm.get('nombreAval')!;
-        const apellidoPaternoAval = this.mainForm.get('apellidoPaternoAval')!;
-        const apellidoMaternoAval = this.mainForm.get('apellidoMaternoAval')!;
-        const telefonoFijoAval = this.mainForm.get('telefonoFijoAval')!;
-        const telefonoMovilAval = this.mainForm.get('telefonoMovilAval')!;
-        const emailAval = this.mainForm.get('emailAval')!;
-        const curpAval = this.mainForm.get('curpAval')!;
-        const tipoCalleAval = this.mainForm.get('tipoCalleAval')!;
-        const nombreCalleAval = this.mainForm.get('nombreCalleAval')!;
-        const numeroExteriorAval = this.mainForm.get('numeroExteriorAval')!;
-        const coloniaAval = this.mainForm.get('coloniaAval')!;
-        const municipioAval = this.mainForm.get('municipioAval')!;
-        const codigoPostalAval = this.mainForm.get('codigoPostalAval')!;
-        const algo = this.mainForm.get('nuevoForm')?.get('algo');
-
-        if (this.camposAval) {
+        if (mostrarCamposAval) {
           nombreAval?.setValidators([Validators.required]);
           apellidoPaternoAval?.setValidators([Validators.required]);
           apellidoMaternoAval?.setValidators([Validators.required]);
@@ -141,11 +139,11 @@ export class NewLoanComponent implements OnInit, OnDestroy {
           numeroExteriorAval?.setValidators([Validators.required]);
           coloniaAval?.setValidators([Validators.required]);
           municipioAval?.setValidators([Validators.required]);
+          estadoAval?.setValidators([Validators.required]);
           codigoPostalAval?.setValidators([
             Validators.required,
             lengthValidator(5),
           ]);
-          algo?.setValidators([Validators.required]);
         } else {
           nombreAval?.clearValidators();
           apellidoPaternoAval?.clearValidators();
@@ -159,32 +157,38 @@ export class NewLoanComponent implements OnInit, OnDestroy {
           numeroExteriorAval?.clearValidators();
           coloniaAval?.clearValidators();
           municipioAval?.clearValidators();
+          estadoAval?.clearValidators();
           codigoPostalAval?.clearValidators();
-          algo?.clearValidators();
         }
 
-        nombreAval?.updateValueAndValidity({ onlySelf: true });
-        apellidoPaternoAval?.updateValueAndValidity({ onlySelf: true });
-        apellidoMaternoAval?.updateValueAndValidity({ onlySelf: true });
-        telefonoFijoAval?.updateValueAndValidity({ onlySelf: true });
-        telefonoMovilAval?.updateValueAndValidity({ onlySelf: true });
-        emailAval?.updateValueAndValidity({ onlySelf: true });
-        curpAval?.updateValueAndValidity({ onlySelf: true });
-        tipoCalleAval?.updateValueAndValidity({ onlySelf: true });
-        nombreCalleAval?.updateValueAndValidity({ onlySelf: true });
-        numeroExteriorAval?.updateValueAndValidity({ onlySelf: true });
-        coloniaAval?.updateValueAndValidity({ onlySelf: true });
-        municipioAval?.updateValueAndValidity({ onlySelf: true });
-        codigoPostalAval?.updateValueAndValidity({ onlySelf: true });
-        algo?.updateValueAndValidity({ onlySelf: true });
+        nombreAval?.updateValueAndValidity();
+        apellidoPaternoAval?.updateValueAndValidity();
+        apellidoMaternoAval?.updateValueAndValidity();
+        telefonoFijoAval?.updateValueAndValidity();
+        telefonoMovilAval?.updateValueAndValidity();
+        emailAval?.updateValueAndValidity();
+        curpAval?.updateValueAndValidity();
+        tipoCalleAval?.updateValueAndValidity();
+        nombreCalleAval?.updateValueAndValidity();
+        numeroExteriorAval?.updateValueAndValidity();
+        coloniaAval?.updateValueAndValidity();
+        municipioAval?.updateValueAndValidity();
+        estadoAval?.updateValueAndValidity();
+        codigoPostalAval?.updateValueAndValidity();
       });
   }
 
   ngOnDestroy(): void {
     this.destroy$.next(null);
     this.destroy$.complete();
+    this.camposAval$.complete();
   }
 
+  /**
+   * Encargado de calcular prestamo cuando se cambia el plazo.
+   *
+   * @param {DropdownChangeEvent} event - El evento propio del componente de primeNg p-dropDown
+   */
   onPlazoChanged({ value }: DropdownChangeEvent) {
     if (!value) return;
     this.semanasDePlazo = value.name;
@@ -193,6 +197,11 @@ export class NewLoanComponent implements OnInit, OnDestroy {
     this.calculaFechaFinal();
   }
 
+  /**
+   * Encargado de calcular prestamo cuando se cambia la cantidad.
+   *
+   * @param {InputNumberInputEvent} event - El evento propio del componente de primeNg p-inputNumber
+   */
   onInputCantidad({ value }: InputNumberInputEvent) {
     if (!value) return;
     this.cantidadIngresada = +value;
@@ -200,15 +209,25 @@ export class NewLoanComponent implements OnInit, OnDestroy {
     this.calculaPrestamo();
   }
 
+  /**
+   * Encargado de calcular el pago semanal y la cantidad total del prestamo.
+   *
+   */
   calculaPrestamo() {
     if (!this.cantidadIngresada || !this.tasaDeInteres) return;
     this.totalAPagar = +(
       this.cantidadIngresada *
       (1 + this.tasaDeInteres / 100)
     ).toFixed(2);
-    this.pagoSemanal = this.totalAPagar / this.semanasDePlazo!;
+    const rawValue = this.totalAPagar / this.semanasDePlazo!;
+    this.pagoSemanal = +rawValue.toFixed(2);
   }
 
+  /**
+   * Encargado de calcular el 'nombre' de la la semana y disparar el calculo de la funcion 'calcularFechaFinal'
+   *
+   * @param {Date} date - La fecha  de la cual se quiere saber el nombre de la semana que le corresponde.
+   */
   calculaDiaDeLaSemana(date: Date) {
     this.fechaInicial = date;
     const dayIndex = this.fechaInicial.getDay();
@@ -216,6 +235,10 @@ export class NewLoanComponent implements OnInit, OnDestroy {
     this.calculaFechaFinal();
   }
 
+  /**
+   * Encargado de calcular la fecha final con un formato facil de leer basado en el plazo y la fecha final definidas en la clase.
+   *
+   */
   calculaFechaFinal() {
     if (!this.fechaInicial || !this.semanasDePlazo) return;
     const result = new Date(this.fechaInicial);
@@ -228,12 +251,18 @@ export class NewLoanComponent implements OnInit, OnDestroy {
     });
   }
 
+  /**
+   * Determina si el elemento de HTML dentro de un FormControl debe ser mostrado o no para el atributo hidden del elemento.
+   *
+   * @param {string} nombreDelCampo - Nombre que tiene el campo dentro del formulario.
+   */
   esCampoOculto(nombreDelCampo: string) {
-    return (
-      this.mainForm.controls[nombreDelCampo].valid ||
-      (!this.mainForm.controls[nombreDelCampo].dirty &&
-        !this.mainForm.controls[nombreDelCampo].touched)
-    );
+    const control = this.mainForm.get(nombreDelCampo);
+    if (!control) {
+      console.warn(`Control ${control} no encontrado`);
+      return false;
+    }
+    return control.valid || (!control.dirty && !control.touched);
   }
 
   onUpload(event: any) {
@@ -274,7 +303,8 @@ export class NewLoanComponent implements OnInit, OnDestroy {
           'Algún dato es incorrecto o aun no se ha proporcionado, revisa nuevamente los campos en el formulario.',
         life: 5000,
       });
-      this.mainForm.markAllAsTouched();
+      //this.mainForm.markAllAsTouched();
+      this.markFormGroupTouched(this.mainForm);
       return;
     }
     this.cs.confirm({
@@ -295,6 +325,20 @@ export class NewLoanComponent implements OnInit, OnDestroy {
     });
   }
 
+  /**
+   * Marca todos los controles anidados para el form proporcionado
+   * @param formGroup - Form a marcar sus inputs como 'tocados'
+   */
+  private markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach((control) => {
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      } else {
+        control.markAsTouched();
+      }
+    });
+  }
+
   onFormAccept() {
     // TODO: esperar a que el backend inserte la informacion,
     // si fue correcto entonces subir documentacion,
@@ -302,9 +346,21 @@ export class NewLoanComponent implements OnInit, OnDestroy {
     this.triggerUpload();
   }
 
+  /**
+   *   Comprueba si hay archivos por subir, si los hay dispara el evento de subida a S3
+   */
   triggerUpload() {
     if (this.fileUpload.hasFiles()) this.fileUpload.upload();
     else console.warn('no habia archivos por subir');
     console.log('triggerUpload');
+  }
+
+  /**
+   * Activa o desactiva el formulario de Aval
+   *
+   * @param {InputSwitchChangeEvent} event - Evento propio del componente de primeNg p-inputSwitch
+   */
+  toggleFormAval(event: InputSwitchChangeEvent) {
+    this.camposAval$.next(event.checked);
   }
 }
