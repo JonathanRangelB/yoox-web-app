22438ca42bc467a3c312eb00791777a8fab33d8c
Jonathan Rangel <jonathanrangelb@gmail.com>
2024-11-21 20:42:06 -0600

    test: agregando el httpclientprovider a los archivos de test de servicios

--- src/app/loan-request/services/s3-bucket.service.spec.ts
+++ src/app/loan-request/services/s3-bucket.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { S3BucketService } from './s3-bucket.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('S3BucketService', () => {
   let service: S3BucketService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(S3BucketService);
   });
 
