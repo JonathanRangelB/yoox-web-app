af27e126d028f2e37bb037fd7cde30af5c74bab4
Jonathan Rangel <jonathanrangelb@gmail.com>
2025-02-11 18:59:55 -0600

    chore: repito

--- src/app/landing/pages/landing.component.ts
+++ src/app/landing/pages/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,10 +7,34 @@ import { Router } from '@angular/router';
   standalone: false,
 })
 export class LandingComponent {
+  readonly #router = inject(Router);
   title = 'YOOX web app';
-  constructor(private router: Router) {}
+  currentYear = new Date().getFullYear();
+
+  carouselItems = [
+    {
+      imageUrl:
+        'https://images.unsplash.com/photo-1579621970563-ebec7560ff3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80',
+      title: 'Planificación financiera',
+    },
+    {
+      imageUrl:
+        'https://images.unsplash.com/photo-1554224155-6726b3ff858f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1711&q=80',
+      title: 'Inversiones inteligentes',
+    },
+    {
+      imageUrl:
+        'https://images.unsplash.com/photo-1434626881859-194d67b2b86f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1774&q=80',
+      title: 'Ahorro para el futuro',
+    },
+    {
+      imageUrl:
+        'https://images.unsplash.com/photo-1450101499163-c8848c66ca85?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80',
+      title: 'Gestión de presupuestos',
+    },
+  ];
 
   navigateTo(path: string) {
-    this.router.navigate([path]);
+    this.#router.navigate([path]);
   }
 }
